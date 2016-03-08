83c26b7b680868966dede0362d0377a920a98a40
w8r <info@w8r.name>
2016-03-08 10:54:34 +0100

    bump leaflet-path-drag

--- dist/Leaflet.draw.drag-src.js
+++ dist/Leaflet.draw.drag-src.js
@@ -119,6 +119,10 @@ L.Path.include({
  */
 L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
 
+  statics: {
+    DRAGGABLE_CLS: 'leaflet-path-draggable'
+  },
+
   /**
    * @param  {L.Path} path
    * @constructor
@@ -145,27 +149,47 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
      */
     this._dragStartPoint = null;
 
+    /**
+     * @type {Boolean}
+     */
+    this._dragInProgress = false;
+
   },
+
 
   /**
    * Enable dragging
    */
   addHooks: function() {
+    var className = L.Handler.PathDrag.DRAGGABLE_CLS;
+    var path      = this._path._path;
+
     this._path.on('mousedown', this._onDragStart, this);
-    if (!L.Path.CANVAS) {
-      L.DomUtil.addClass(this._path._container, 'leaflet-path-draggable');
+    this._path.options.className =
+      (this._path.options.className || '') + ' ' + className;
+
+    if (!L.Path.CANVAS && path) {
+      L.DomUtil.addClass(path, className);
     }
   },
+
 
   /**
    * Disable dragging
    */
   removeHooks: function() {
+    var className = L.Handler.PathDrag.DRAGGABLE_CLS;
+    var path      = this._path._path;
+
     this._path.off('mousedown', this._onDragStart, this);
-    if (!L.Path.CANVAS) {
-      L.DomUtil.removeClass(this._path._container, 'leaflet-path-draggable');
+    this._path.options.className =
+      (this._path.options.className || '').replace(className, '');
+
+    if (!L.Path.CANVAS && path) {
+      L.DomUtil.removeClass(path, className);
     }
   },
+
 
   /**
    * @return {Boolean}
@@ -174,11 +198,23 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
     return this._path._dragMoved;
   },
 
+
+  /**
+   * If dragging currently in progress.
+   *
+   * @return {Boolean}
+   */
+  inProgress: function() {
+    return this._dragInProgress;
+  },
+
+
   /**
    * Start drag
    * @param  {L.MouseEvent} evt
    */
   _onDragStart: function(evt) {
+    this._dragInProgress = true;
     this._startPoint = evt.containerPoint.clone();
     this._dragStartPoint = evt.containerPoint.clone();
     this._matrix = [1, 0, 0, 1, 0, 0];
@@ -188,6 +224,7 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
       .on('mouseup', this._onDragEnd, this)
     this._path._dragMoved = false;
   },
+
 
   /**
    * Dragging
@@ -221,12 +258,14 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
     L.DomEvent.stop(evt.originalEvent);
   },
 
+
   /**
    * Dragging stopped, apply
    * @param  {L.MouseEvent} evt
    */
   _onDragEnd: function(evt) {
     L.DomEvent.stop(evt);
+    this._dragInProgress = false;
     // undo container transform
     this._path._resetTransform();
     // apply matrix
@@ -254,6 +293,30 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
     this._dragStartPoint = null;
     this._path._dragMoved = false;
   },
+
+
+  /**
+   * Transforms point according to the provided transformation matrix.
+   *
+   *  @param {Array.<Number>} matrix
+   *  @param {L.LatLng} point
+   */
+  _transformPoint: function(point, matrix) {
+    var path = this._path;
+
+    var px = L.point(matrix[4], matrix[5]);
+
+    var crs = path._map.options.crs;
+    var transformation = crs.transformation;
+    var scale = crs.scale(path._map.getZoom());
+    var projection = crs.projection;
+
+    var diff = transformation.untransform(px, scale)
+      .subtract(transformation.untransform(L.point(0, 0), scale));
+
+    return projection.unproject(projection.project(point)._add(diff));
+  },
+
 
   /**
    * Applies transformation, does it in one sweep for performance,
@@ -313,10 +376,9 @@ L.Handler.PathDrag = L.Handler.extend( /** @lends  L.Path.Drag.prototype */ {
 
 });
 
-L.Path.prototype.__initEvents = L.Path.prototype._initEvents;
-L.Path.prototype._initEvents = function() {
-  this.__initEvents();
 
+// Init hook instead of replacing the `initEvents`
+L.Path.addInitHook(function() {
   if (this.options.draggable) {
     if (this.dragging) {
       this.dragging.enable();
@@ -327,7 +389,41 @@ L.Path.prototype._initEvents = function() {
   } else if (this.dragging) {
     this.dragging.disable();
   }
+});
+
+/*
+ * Return transformed points in case if dragging is enabled and in progress,
+ * otherwise - call original method.
+ *
+ * For L.Circle and L.Polyline
+ */
+
+// don't like this? me neither, but I like it even less
+// when the original methods are not exposed
+L.Circle.prototype._getLatLng = L.Circle.prototype.getLatLng;
+L.Circle.prototype.getLatLng = function() {
+  if (this.dragging && this.dragging.inProgress()) {
+    return this.dragging._transformPoint(this._latlng, this.dragging._matrix);
+  } else {
+    return this._getLatLng();
+  }
 };
+
+
+L.Polyline.prototype._getLatLngs = L.Polyline.prototype.getLatLngs;
+L.Polyline.prototype.getLatLngs = function() {
+  if (this.dragging && this.dragging.inProgress()) {
+    var matrix = this.dragging._matrix;
+    var points = this._getLatLngs();
+    for (var i = 0, len = points.length; i < len; i++) {
+      points[i] = this.dragging._transformPoint(points[i], matrix);
+    }
+    return points;
+  } else {
+    return this._getLatLngs();
+  }
+};
+
 (function() {
 
   // listen and propagate dragstart on sub-layers
